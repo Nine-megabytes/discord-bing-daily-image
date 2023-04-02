8a6cdde230def2d3a32fb18092f02c8bc4b77cc0
半年前的自己 <64721413+BNQDZJ@users.noreply.github.com>
2023-04-02 12:47:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,15 +9,17 @@ bing_url = os.environ["bing_url"]
 response = requests.get(bing_url)
 data = json.loads(response.text)
 bing_image_url = 'https://www.bing.com' + data['images'][0]['urlbase']+"_UHD.jpg"
+bing_copyright = data['images'][0]['copyright']
+bing_copyrightlink = data['images'][0]['copyrightlink']
 
 # 添加时间戳
 time_stamp = time.time()
 utc_time = datetime.utcfromtimestamp(time_stamp)
-time1 = str(utc_time + timedelta(hours=-7))
+time1 = str(utc_time + timedelta(hours=+8))
 
 # 定义要发送的消息
 message = {
-      "content": "@everyone This is the Bing Daily image"+"\n Time(UTC-7):"+time1,
+      "content": "@everyone This is the Bing Daily image"+"\n Time(UTC+8):"+time1,
       "embeds": [
         {
           "image": {
@@ -26,6 +28,10 @@ message = {
         }
       ]
     }
+
+message_bing_copyright = {
+    "content": bing_copyright + "\n" + bing_copyrightlink,
+}
 
 # 定义请求头，包括机器人令牌和消息类型
 bot_token = os.environ["bot_token"]
@@ -40,6 +46,7 @@ url = discord_url
 
 # 发送 POST 请求，包括要发送的消息和请求头
 response = requests.post(url, data=json.dumps(message), headers=headers)
+response_bing_copyright = requests.post(url, data=json.dumps(message_bing_copyright), headers=headers)
 
 # 打印响应
 print(response.text)
